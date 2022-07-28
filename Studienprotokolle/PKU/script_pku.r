6a95ec8447d99285abc7b3578bd6c498100b762c
Gierschner, Christian <christian.gierschner@ukdd.de>
2022-07-28 16:49:22 +0200

    update pku script and add new conf.yml.sample

--- Studienprotokolle/PKU/script_pku.r
+++ Studienprotokolle/PKU/script_pku.r
@@ -10,32 +10,73 @@ if (!require("dplyr")) install.packages("dplyr")
 
 conf <- config::get(file = paste(getwd(), "/conf.yml", sep = ""))
 #check for proxy configuration
-if (nchar(conf$http_proxy) >= 1) {
-  Sys.setenv(http_proxy = conf$http_proxy)
+if (exists("http_proxy", where = conf)) {
+  if (nchar(conf$http_proxy) >= 1) {
+    Sys.setenv(http_proxy = conf$http_proxy)
+  }
 }
-if (nchar(conf$https_proxy) >= 1) {
-  Sys.setenv(https_proxy = conf$https_proxy)
+
+if (exists("https_proxy", where = conf)) {
+  if (nchar(conf$https_proxy) >= 1) {
+    Sys.setenv(https_proxy = conf$https_proxy)
+  }
 }
-if (nchar(conf$no_proxy) >= 1) {
-  Sys.setenv(no_proxy = conf$no_proxy)
+
+if (exists("count", where = conf)) {
+  if (nchar(conf$no_proxy) >= 1) {
+    Sys.setenv(no_proxy = conf$no_proxy)
+  }
 }
 
 # check for custom recordedDate
-if (nchar(conf$recordedDate_col) >= 1) {
-  recorded_date_custom <- conf$recordedDate_col
+if (exists("recordedDate_col", where = conf)) {
+  if (nchar(conf$recordedDate_col) >= 1) {
+    recorded_date_custom <- conf$recordedDate_col
+  } else {
+    recorded_date_custom <- "recordedDate"
+  }
+  search_date <- paste0("&", strsplit(recorded_date_custom, "Date")[[1]][1],  "-date=gt2014-12-31")
 } else {
   recorded_date_custom <- "recordedDate"
+  search_date <- paste0("&", strsplit(recorded_date_custom, "Date")[[1]][1],  "-date=gt2014-12-31")
 }
 
+
 # check for custom icd_code_system
-if (nchar(conf$icd_code_system) >= 1) {
-  icd_code_system_custom <- conf$icd_code_system
+if (exists("icd_code_system", where = conf)) {
+  if (nchar(conf$icd_code_system) >= 1) {
+    icd_code_system_custom <- conf$icd_code_system
+  } else {
+    icd_code_system_custom <- "http://fhir.de/CodeSystem/dimdi/icd-10-gm"
+  }
 } else {
   icd_code_system_custom <- "http://fhir.de/CodeSystem/dimdi/icd-10-gm"
 }
 
-if (!(conf$ssl_verify_peer)) {
-  httr::set_config(httr::config(ssl_verifypeer = 0L))
+if (exists("ssl_verify_peer", where = conf)) {
+  if (!(conf$ssl_verify_peer)) {
+    httr::set_config(httr::config(ssl_verifypeer = 0L))
+  }
+}
+
+if (exists("max_bundles", where = conf)) {
+  if (nchar(conf$max_bundles) >= 1) {
+    max_bundles_custom <- conf$max_bundles
+  } else {
+    max_bundles_custom <- 0
+  }
+} else {
+  max_bundles_custom <- 0
+}
+
+if (exists("count", where = conf)) {
+  if (nchar(conf$count) >= 1) {
+    count_custom <- paste0("&_count=", conf$count)
+  } else {
+    count_custom <- ""
+  }
+} else {
+  count_custom <- ""
 }
 
 search_request <- paste0(
@@ -83,9 +124,24 @@ search_request <- paste0(
   ",O80%20Z37.9%21,O81%20Z37.9%21,O82%20Z37.9%21",
   ",J18.0,J18.1,J18.2,J18.8,J18.9",
   ",Z38.0,Z38.1,Z38.2,Z38.3,Z38.4,Z38.5,Z38.6,Z38.7,Z38.8",
-  "&_include=Condition:subject:Patient")
+  search_date,
+  count_custom,
+  "&_include=Condition:subject:Patient"
+  )
 
-condition_patient_bundle <- fhir_search(request = search_request, username = conf$user, password = conf$password, verbose = 2, max_bundles = 0)
+if (exists("token", where = conf)) {
+  if (nchar(conf$token) >= 1) {
+    condition_patient_bundle <- fhir_search(request = search_request, username = conf$user, password = conf$password, token = conf$token, verbose = 2, max_bundles = max_bundles_custom)
+    } else {
+    condition_patient_bundle <- fhir_search(request = search_request, username = conf$user, password = conf$password, verbose = 2, max_bundles = max_bundles_custom)
+    }
+  } else {
+    if (exists("username", where = conf)) {
+      condition_patient_bundle <- fhir_search(request = search_request, username = conf$user, password = conf$password, verbose = 2, max_bundles = max_bundles_custom)
+    } else {
+      condition_patient_bundle <- fhir_search(request = search_request, verbose = 2, max_bundles = max_bundles_custom)
+    }
+}
 
 conditions <- fhir_table_description(resource = "Condition",
                                      cols = c(diagnosis = "code/coding/code",
@@ -126,10 +182,13 @@ df_conditions_tmp <- fhir_melt(df_conditions_raw,
 
 # unnest raw conditions dataframe columns diagnosis, system
 df_conditions_tmp <- fhir_melt(df_conditions_tmp,
-                               columns = c("diagnosis", "display", "system", "patient_id"),
+                               columns = c("diagnosis", "display", "system"),
                                brackets = c("[", "]"), sep = "|", all_columns = TRUE)
 
 df_conditions_tmp <- fhir_rm_indices(df_conditions_tmp, brackets = c("[", "]"))
+
+# filter conditions by system to obtain only icd-10-gm system
+df_conditions_tmp <- df_conditions_tmp[df_conditions_tmp$system == icd_code_system_custom, ]
 
 # remove the "Patient/" tag from column patient_id in condition resource
 df_conditions_tmp$patient_id <- sub("Patient/", "", df_conditions_tmp[, "patient_id"])
@@ -141,7 +200,7 @@ df_patients_tmp <- fhir_melt(df_patients_raw,
 df_patients_tmp <- fhir_rm_indices(df_patients_tmp, brackets = c("[", "]"))
 
 # remove all male patients (maybe remove for male children)
-#df_patients_tmp <- df_patients_tmp[df_patients_tmp$gender != "male",]
+#df_patients_tmp <- df_patients_tmp[df_patients_tmp$gender != "male", ]
 
 # remove duplicate entries
 df_patients_tmp <- df_patients_tmp[!duplicated(df_patients_tmp$patient_id), ]
@@ -151,8 +210,15 @@ x <- c(1, 17, 31, 99)
 # merge conditions and patients dataframe
 df_conditions_patients <- base::merge(df_conditions_tmp, df_patients_tmp, by = "patient_id")
 
+df_conditions_patients$recorded_date <- as.Date(df_conditions_patients$recorded_date, format = "%Y-%m-%d")
+df_conditions_patients <- df_conditions_patients[df_conditions_patients$recorded_date > "2014-12-31", ]
+df_conditions_patients <- df_conditions_patients[df_conditions_patients$recorded_date < "2022-12-31", ]
+
 # remove merge identifier column as its not needed and could cause problems
 df_conditions_patients <- df_conditions_patients %>% select(-contains("resource_identifier"))
+
+# check if birthdate is in format YYYY-MM-DD, if not append -01-01
+df_conditions_patients <- mutate(df_conditions_patients, birthdate = ifelse(nchar(df_conditions_patients$birthdate) >= 10, df_conditions_patients$birthdate, paste0(df_conditions_patients$birthdate, "-01-01")))
 
 # calculate age as of recorded_date - birthdate
 df_conditions_patients$age <- round(as.double(as.Date(df_conditions_patients$recorded_date) - as.Date(df_conditions_patients$birthdate)) / 365.25, 0)
@@ -162,96 +228,102 @@ df_conditions_patients$age_group <- cut(df_conditions_patients$age, x, breaks = 
 
 # filter conditions for ICD-Code E70*
 df_conditions_pku <- subset(df_conditions_patients, grepl("^E70", diagnosis))
-df_conditions_pku <- unique(df_conditions_pku[, c(1, 2, 3, 4, 5)])
-
-#----
-#df_conditions_pku <- base::merge(df_conditions_pku, df_patients_tmp, by = "patient_id")
-#df_conditions_pku$age <- round(as.double(as.Date(df_conditions_pku$recorded_date) - as.Date(df_conditions_pku$birthdate)) / 365.25, 0)
-#df_conditions_pku$age_group <- cut(df_conditions_pku$age, x, breaks = c(0, 17, 30, 99), labels = c("[0,17]", "[18,30]", "[31,99]"))
-
-#df_conditions_pku <- df_conditions_pku[!duplicated(df_conditions_pku$patient_id),]
-#df_conditions_pku1 <- base::merge(df_conditions_pku, df_patients_tmp, by = "patient_id")
-#df_conditions_pku1$age <- round( as.double( as.Date( df_conditions_pku1$recorded_date ) - as.Date( df_conditions_pku1$birthdate ) ) / 365.25, 0 )
-
-#df_conditions_pku <- unique(df_conditions_pku[, c(1,2,3,4,5)])
-#df_b <- base::merge(df_conditions_pku, df_a, by = "patient_id")
-#df_b <- df_b %>% select(-contains(".y"))
-#----
+#df_conditions_pku <- unique(df_conditions_pku[, c(1, 2, 3, 4, 5)])
 
 df_conditions_pku_0 <- subset(df_conditions_pku, grepl("^E70.0", diagnosis))
-#df_conditions_pku_0$recorded_date <- as.Date(df_conditions_pku_0$recorded_date, format= "%Y-%m-%d")
-#df_conditions_pku_0 <- df_conditions_pku_0[df_conditions_pku_0$recorded_date < "2019-12-31",]
 
 df_conditions_pku_1 <- subset(df_conditions_pku, grepl("^E70.1", diagnosis))
-#df_conditions_pku_1$recorded_date <- as.Date(df_conditions_pku_1$recorded_date, format= "%Y-%m-%d")
-#df_conditions_pku_1 <- df_conditions_pku_1[df_conditions_pku_1$recorded_date < "2019-12-31",]
 
-df_conditions_n <- subset(df_conditions_patients, grepl("^N18", diagnosis))
-#df_conditions_n$recorded_date <- as.Date(df_conditions_n$recorded_date, format= "%Y-%m-%d")
-#df_conditions_n <- df_conditions_n[df_conditions_n$recorded_date < "2019-12-31",]
+df_conditions_n18 <- subset(df_conditions_patients, grepl("^N18", diagnosis))
 
-df_conditions_birth_all <- subset(df_conditions_patients, grepl("^O|^Z", diagnosis))
-#df_conditions_birth_all$recorded_date <- as.Date(df_conditions_birth_all$recorded_date, format= "%Y-%m-%d")
-#df_conditions_birth_all <- df_conditions_birth_all[df_conditions_birth_all$recorded_date < "2019-12-31",]
+df_pku_0_n <- base::merge(df_conditions_pku_0, df_conditions_n18, by = "patient_id")
+df_pku_0_n <- df_pku_0_n[!duplicated(df_pku_0_n$patient_id), ]
+df_pku_0_n <- df_pku_0_n %>% select(-contains("resource_identifier"))
 
-df_pku_0_N <- base::merge(df_conditions_pku_0, df_conditions_n, by = "patient_id")
-#df_pku_0_N <- base::merge(df_pku_0_N, df_patients_tmp, by.x = "patient_id",by.y = "patient_id")
-#df_pku_0_N <- df_pku_0_N[!duplicated(df_pku_0_N$patient_id),]
-#df_pku_0_N$age <- round( as.double( as.Date( df_pku_0_N$recorded_date.y ) - as.Date( df_pku_0_N$birthdate ) ) / 365.25, 0 )
-#df_pku_0_N$age_group <- cut(df_pku_0_N$age,x,breaks= c(0,17,30,99), labels = c("[0,17]","[18,30]","[31,99]"))
-df_pku_0_N <- df_pku_0_N %>% select(-contains("resource_identifier"))
+df_pku_1_n <- base::merge(df_conditions_pku_1, df_conditions_n18, by = "patient_id")
+df_pku_1_n <- df_pku_1_n[!duplicated(df_pku_1_n$patient_id), ]
+df_pku_1_n <- df_pku_1_n %>% select(-contains("resource_identifier"))
 
-df_pku_1_N <- base::merge(df_conditions_pku_1, df_conditions_n, by = "patient_id")
-#df_pku_1_N <- base::merge(df_pku_1_N, df_patients_tmp, by.x = "patient_id",by.y = "patient_id")
-df_pku_1_N <- df_pku_1_N[!duplicated(df_pku_1_N$patient_id), ]
-#df_pku_1_N$age <- round( as.double( as.Date( df_pku_1_N$recorded_date.y ) - as.Date( df_pku_1_N$birthdate ) ) / 365.25, 0 )
-#df_pku_1_N$age_group <- cut(df_pku_1_N$age,x,breaks= c(0,17,30,99), labels = c("[0,17]","[18,30]","[31,99]"))
-df_pku_1_N <- df_pku_1_N %>% select(-contains("resource_identifier"))
+df_conditions_f32 <- subset(df_conditions_patients, grepl("^F32", diagnosis))
 
-df_pku_N <- rbind(df_pku_0_N, df_pku_1_N)
-df_result_primaer <- as.data.frame(df_pku_N %>% group_by(Einrichtungsindikator = df_pku_N$hospital_id, Diagn1 = df_pku_N$diagnosis.x, Diagn2 = df_pku_N$diagnosis.y, Geschlecht = df_pku_N$gender, Alter = df_pku_N$age_group) %>% summarise(Anzahl = n()) %>% mutate(Haeufigkeit = paste0(round(100 * Anzahl / sum(Anzahl), 0), "%")))
+df_pku_0_f32 <- base::merge(df_conditions_pku_0, df_conditions_f32, by = "patient_id")
+df_pku_0_f32 <- df_pku_0_f32[!duplicated(df_pku_0_f32$patient_id), ]
+df_pku_0_f32 <- df_pku_0_f32 %>% select(-contains("resource_identifier"))
 
+df_pku_1_f32 <- base::merge(df_conditions_pku_1, df_conditions_f32, by = "patient_id")
+df_pku_1_f32 <- df_pku_1_f32[!duplicated(df_pku_1_f32$patient_id), ]
+df_pku_1_f32 <- df_pku_1_f32 %>% select(-contains("resource_identifier"))
 
-if (nrow(df_pku_N) == 0) {
+df_conditions_f33 <- subset(df_conditions_patients, grepl("^F33", diagnosis))
+
+df_pku_0_f33 <- base::merge(df_conditions_pku_0, df_conditions_f33, by = "patient_id")
+df_pku_0_f33 <- df_pku_0_f33[!duplicated(df_pku_0_f33$patient_id), ]
+df_pku_0_f33 <- df_pku_0_f33 %>% select(-contains("resource_identifier"))
+
+df_pku_1_f33 <- base::merge(df_conditions_pku_1, df_conditions_f33, by = "patient_id")
+df_pku_1_f33 <- df_pku_1_f33[!duplicated(df_pku_1_f33$patient_id), ]
+df_pku_1_f33 <- df_pku_1_f33 %>% select(-contains("resource_identifier"))
+
+df_conditions_f34 <- subset(df_conditions_patients, grepl("^F34", diagnosis))
+
+df_pku_0_f34 <- base::merge(df_conditions_pku_0, df_conditions_f34, by = "patient_id")
+df_pku_0_f34 <- df_pku_0_f34[!duplicated(df_pku_0_f34$patient_id), ]
+df_pku_0_f34 <- df_pku_0_f34 %>% select(-contains("resource_identifier"))
+
+df_pku_1_f34 <- base::merge(df_conditions_pku_1, df_conditions_f34, by = "patient_id")
+df_pku_1_f34 <- df_pku_1_f34[!duplicated(df_pku_1_f34$patient_id), ]
+df_pku_1_f34 <- df_pku_1_f34 %>% select(-contains("resource_identifier"))
+
+df_conditions_g31 <- subset(df_conditions_patients, grepl("^G31", diagnosis))
+
+df_pku_0_g31 <- base::merge(df_conditions_pku_0, df_conditions_g31, by = "patient_id")
+df_pku_0_g31 <- df_pku_0_g31[!duplicated(df_pku_0_g31$patient_id), ]
+df_pku_0_g31 <- df_pku_0_g31 %>% select(-contains("resource_identifier"))
+
+df_pku_1_g31 <- base::merge(df_conditions_pku_1, df_conditions_g31, by = "patient_id")
+df_pku_1_g31 <- df_pku_1_g31[!duplicated(df_pku_1_g31$patient_id), ]
+df_pku_1_g31 <- df_pku_1_g31 %>% select(-contains("resource_identifier"))
+
+df_pku_result_primaer <- rbind(df_pku_0_n, df_pku_1_n, df_pku_0_f32, df_pku_1_f32, df_pku_0_f33, df_pku_1_f33, df_pku_0_f34, df_pku_1_f34, df_pku_0_g31, df_pku_1_g31)
+df_result_primaer <- as.data.frame(df_pku_result_primaer %>% group_by(Einrichtungsindikator = df_pku_result_primaer$hospital_id.x, Diagn1 = df_pku_result_primaer$diagnosis.x, Diagn2 = df_pku_result_primaer$diagnosis.y, Geschlecht = df_pku_result_primaer$gender.x, Alter = df_pku_result_primaer$age_group.x) %>% summarise(Anzahl = n()) %>% mutate(Haeufigkeit = paste0(round(100 * n() / sum(n()), 0), "%")))
+#df_result_primaer <- mutate(df_result_primaer, Anzahl = ifelse(Anzahl > 0 & Anzahl <= 5, "<5", Anzahl))
+
+if (nrow(df_pku_result_primaer) == 0) {
   result_sekundaer_a <- 0
 } else {
-  result_sekundaer_a <- round(nrow(df_pku_N) / nrow(df_conditions_pku) * 100)
+  result_sekundaer_a <- round(nrow(df_pku_result_primaer) / nrow(df_conditions_pku[!duplicated(as.numeric(df_conditions_pku$patient_id)), ]) * 100)
 }
+
+df_conditions_birth_all <- subset(df_conditions_patients, grepl("^O|^Z", diagnosis))
 
 df_conditions_birth <- subset(df_conditions_patients, grepl("^O09|^O3|^O63|^O8|^Z", diagnosis))
 
 df_pku_birth <- base::merge(df_conditions_pku, df_conditions_birth, by = "patient_id")
-#df_pku_birth <- base::merge(df_pku_birth, df_patients_tmp, by.x = "patient_id",by.y = "patient_id")
 df_pku_birth <- df_pku_birth[!duplicated(df_pku_birth$patient_id), ]
-#df_pku_birth$age <- round( as.double( as.Date( df_pku_birth$recorded_date.y ) - as.Date( df_pku_birth$birthdate ) ) / 365.25, 0 )
-#df_pku_birth$age_group <- cut(df_pku_birth$age,x,breaks= c(0,17,30,99), labels = c("[0,17]","[18,30]","[31,99]"))
-df_pku_birth <- df_pku_birth[df_pku_birth$age > 14, ]
+df_pku_birth <- subset(df_pku_birth, !grepl("^Z38", diagnosis.y))
 df_pku_birth <- df_pku_birth %>% select(-contains("resource_identifier"))
-
-#df_result_sekundaer_c <- as.data.frame(df_pku_birth%>%group_by(Einrichtungsindikator=df_pku_birth$hospital_id,Diagn1=df_pku_birth$diagnosis.x,Diagn2=df_pku_birth$diagnosis.y,Geschlecht=df_pku_birth$gender,Alter=df_pku_birth$age_group)%>%summarise(Anzahl=n()))
 
 df_conditions_complication <- subset(df_conditions_patients, grepl("^O64|^O75|^O24", diagnosis))
 
 df_pku_complication <- base::merge(df_conditions_pku, df_conditions_birth, by = "patient_id")
 df_pku_complication <- base::merge(df_pku_complication, df_conditions_complication, by = "patient_id")
-#df_pku_complication <- base::merge(df_pku_complication, df_patients_tmp, by.x = "patient_id",by.y = "patient_id")
 df_pku_complication <- df_pku_complication[!duplicated(df_pku_complication$patient_id), ]
-#df_pku_complication$age <- round( as.double( as.Date( df_pku_complication$recorded_date.y ) - as.Date( df_pku_complication$birthdate ) ) / 365.25, 0 )
-#df_pku_complication$age_group <- cut(df_pku_complication$age,x,breaks= c(0,17,30,99), labels = c("[0,17]","[18,30]","[31,99]"))
-#df_pku_complication <- df_pku_complication[df_pku_complication$age > 14,]
+df_pku_complication <- subset(df_pku_complication, !grepl("^Z38", diagnosis.y))
 df_pku_complication <- df_pku_complication %>% select(-contains("resource_identifier"))
 
-df_result_sekundaer_c <- as.data.frame(df_pku_complication %>% group_by(Einrichtungsindikator = df_pku_complication$hospital_id.x, Diagn1 = df_pku_complication$diagnosis.x, Diagn2 = df_pku_complication$diagnosis, Geschlecht = df_pku_complication$gender.x, Alter = df_pku_complication$age_group.x) %>% summarise(Anzahl = n()))
+df_result_sekundaer_c <- as.data.frame(df_pku_complication %>% group_by(Einrichtungsindikator = df_pku_complication$hospital_id.x, Diagn1 = df_pku_complication$diagnosis.x, Diagn2 = df_pku_complication$diagnosis, Geschlecht = df_pku_complication$gender.x, Alter = df_pku_complication$age_group.x) %>% summarise(Anzahl = n()) %>% mutate(Haeufigkeit = paste0(round(100 * n() / sum(n()), 0), "%")))
+#df_result_sekundaer_c <- mutate(df_result_sekundaer_c, Anzahl = ifelse(Anzahl > 0 & Anzahl <= 5, "<5", Anzahl))
 
 # display the final output
 df_result_primaer
 paste0(result_sekundaer_a, "%")
 df_result_sekundaer_c
+
 ########################################################################################################################################################
 # write result to a csv file
 ########################################################################################################################################################
 write.csv(df_result_primaer, file = "result_primaer.csv", row.names = FALSE)
 write.csv(result_sekundaer_a, file = "result_sekundaer_a.csv", row.names = FALSE)
-write.csv(df_result_sekundaer_b, file = "result_sekundaer_b.csv", row.names = FALSE)
+#write.csv(df_result_sekundaer_b, file = "result_sekundaer_b.csv", row.names = FALSE)
 write.csv(df_result_sekundaer_c, file = "result_sekundaer_b.csv", row.names = FALSE)
 ########################################################################################################################################################
